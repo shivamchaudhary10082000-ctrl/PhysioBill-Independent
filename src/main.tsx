9baa38f4c46c11c40de5ccbb3fb0832a6b1f8ae8
shivamchaudhary10082000-ctrl <shivamchaudhary10082000@gmail.com>
2026-08-19 00:51:14 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
-import { ErrorBoundary } from '@/components/error-boundary';
+import { ErrorBoundary } from '@/Components/error-boundary';
 
 import './index.css';
 
